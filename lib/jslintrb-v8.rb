fb816052486f5ccf266def4e7f07848e9a1ab1cc
Rebecca Murphey <rmurphey@gmail.com>
2011-03-05 16:33:42 -0500

    DRY pass

--- lib/jslintrb-v8.rb
+++ lib/jslintrb-v8.rb
@@ -2,7 +2,7 @@ require 'v8'
 # JSLint bindings for ruby using v8.
 #
 # Usage:
-# 
+#
 #    require 'jslintrb-v8'
 #    puts JSLint.new.check("var x = 5")
 #
@@ -42,163 +42,97 @@ require 'v8'
 #   end
 #
 class JSLint
-    # if ADsafe should be enforced
-    attr_accessor :adsafe   
-    # if bitwise operators should not be allowed
-    attr_accessor :bitwise  
-    # if the standard browser globals should be predefined
-    attr_accessor :browser  
-    # if upper case HTML should be allowed
-    attr_accessor :cap      
-    # if CSS workarounds should be tolerated
-    attr_accessor :css      
-    # if debugger statements should be allowed
-    attr_accessor :debug    
-    # if === should be required
-    attr_accessor :eqeqeq   
-    # if eval should be allowed
-    attr_accessor :evil     
-    # if for in statements must filter
-    attr_accessor :forin    
-    # if HTML fragments should be allowed
-    attr_accessor :fragment 
-    # if immediate invocations must be wrapped in parens
-    attr_accessor :immed    
-    # if line breaks should not be checked
-    attr_accessor :laxbreak 
-    # if constructor names must be capitalized
-    attr_accessor :newcap   
-    # if names should be checked
-    attr_accessor :nomen    
-    # if HTML event handlers should be allowed
-    attr_accessor :on       
-    # if only one var statement per function should be allowed
-    attr_accessor :onevar   
-    # if the scan should stop on first error
-    attr_accessor :passfail 
-    # if increment/decrement should not be allowed
-    attr_accessor :plusplus 
-    # if the . should not be allowed in regexp literals
-    attr_accessor :regexp   
-    # if the Rhino environment globals should be predefined
-    attr_accessor :rhino    
-    # if variables should be declared before used
-    attr_accessor :undef    
-    # if use of some browser features should be restricted
-    attr_accessor :safe     
-    # if the System object should be predefined
-    attr_accessor :sidebar  
-    # require the "use strict"; pragma
-    attr_accessor :strict   
-    # if all forms of subscript notation are tolerated
-    attr_accessor :sub      
-    # if strict whitespace rules apply
-    attr_accessor :white    
-    # if the Yahoo Widgets globals should be predefined
-    attr_accessor :widget   
-
   def initialize(opts = {})
-    @adsafe   = opts.fetch( :adsafe   ){ false } 
-    @bitwise  = opts.fetch( :bitwise  ){ true  } 
-    @browser  = opts.fetch( :browser  ){ false } 
-    @cap      = opts.fetch( :cap      ){ false } 
-    @css      = opts.fetch( :css      ){ false } 
-    @debug    = opts.fetch( :debug    ){ false } 
-    @eqeqeq   = opts.fetch( :eqeqeq   ){ true  } 
-    @evil     = opts.fetch( :evil     ){ false } 
-    @forin    = opts.fetch( :forin    ){ false } 
-    @fragment = opts.fetch( :fragment ){ false } 
-    @immed    = opts.fetch( :immed    ){ true  } 
-    @laxbreak = opts.fetch( :laxbreak ){ false } 
-    @newcap   = opts.fetch( :newcap   ){ true  } 
-    @nomen    = opts.fetch( :nomen    ){ true  } 
-    @on       = opts.fetch( :on       ){ false } 
-    @onevar   = opts.fetch( :onevar   ){ true  } 
-    @passfail = opts.fetch( :passfail ){ false } 
-    @plusplus = opts.fetch( :plusplus ){ true  } 
-    @regexp   = opts.fetch( :regexp   ){ true  } 
-    @rhino    = opts.fetch( :rhino    ){ false } 
-    @undef    = opts.fetch( :undef    ){ true  } 
-    @safe     = opts.fetch( :safe     ){ false } 
-    @sidebar  = opts.fetch( :sidebar  ){ false } 
-    @strict   = opts.fetch( :strict   ){ true  } 
-    @sub      = opts.fetch( :sub      ){ false } 
-    @white    = opts.fetch( :white    ){ false } 
-    @widget   = opts.fetch( :widget   ){ false } 
+    # default jslint settings
+    @settings = {
+      # if ADsafe should be enforced
+      :adsafe     => false,
+      # if bitwise operators should not be allowed
+      :bitwise    => true,
+      # if the standard browser globals should be predefined
+      :browser    => false,
+      # if upper case HTML should be allowed
+      :cap        => false,
+      # if CSS workarounds should be tolerated
+      :css        => false,
+      # if debugger statements should be allowed
+      :debug      => false,
+      # if === should be required
+      :eqeqeq     => true,
+      # if eval should be allowed
+      :evil       => false,
+      # if for in statements must filter
+      :forin      => false,
+      # if HTML fragments should be allowed
+      :fragment   => false,
+      # if immediate invocations must be wrapped in parens
+      :immed      => true,
+      # if line breaks should not be checked
+      :laxbreak   => false,
+      # if constructor names must be capitalized
+      :newcap     => true,
+      # if names shoudl be checked
+      :nomen      => true,
+      # if HTML event handlers should be allowed
+      :on         => false,
+      # if only one var statement per function should be allowed
+      :onevar     => true,
+      # if the scan should stop on first error
+      :passfail   => false,
+      # if increment/decrement should not be allowed
+      :plusplus   => true,
+      # if the . should not be allowed in regexp literals
+      :regexp     => true,
+      # if the Rhino environment globals should be predefined
+      :rhino      => false,
+      # if variables should be declared before used
+      :undef      => true,
+      # if use of some browser features should be restricted
+      :safe       => false,
+      # if the System object should be predefined
+      :sidebar    => false,
+      # require the "use strict"; pragma
+      :strict     => true,
+      # if all forms of subscript notation are tolerated
+      :sub        => false,
+      # if strict whitespace rules apply
+      :white      => false,
+      # if the Yahoo Widgets globals should be predefined
+      :widget     => false
+    }
+
+    # override default settings with passed in options
+    @settings.merge(opts);
   end
 
   def check(input)
     errors = []
+
     V8::Context.new do |context|
       context.load(File.join(File.dirname(__FILE__), 'jslintrb-v8', 'jslint.js'))
 
-      context['JSLintRBinput']    = lambda{ input }
-      context['JSLintRBadsafe']   = @adsafe
-      context['JSLintRBbitwise']  = @bitwise 
-      context['JSLintRBbrowser']  = @browser 
-      context['JSLintRBcap']      = @cap 
-      context['JSLintRBcss']      = @css 
-      context['JSLintRBdebug']    = @debug 
-      context['JSLintRBeqeqeq']   = @eqeqeq 
-      context['JSLintRBevil']     = @evil 
-      context['JSLintRBforin']    = @forin 
-      context['JSLintRBfragment'] = @fragment 
-      context['JSLintRBimmed']    = @immed 
-      context['JSLintRBlaxbreak'] = @laxbreak 
-      context['JSLintRBnewcap']   = @newcap 
-      context['JSLintRBnomen']    = @nomen 
-      context['JSLintRBon']       = @on 
-      context['JSLintRBonevar']   = @onevar 
-      context['JSLintRBpassfail'] = @passfail 
-      context['JSLintRBplusplus'] = @plusplus 
-      context['JSLintRBregexp']   = @regexp 
-      context['JSLintRBrhino']    = @rhino 
-      context['JSLintRBundef']    = @undef 
-      context['JSLintRBsafe']     = @safe 
-      context['JSLintRBsidebar']  = @sidebar 
-      context['JSLintRBstrict']   = @strict 
-      context['JSLintRBsub']      = @sub 
-      context['JSLintRBwhite']    = @white 
-      context['JSLintRBwidget']   = @widget 
-      context['JSLintRBreportErrors'] = lambda{|js_errors|
+      # prep the context object
+      @settings.each do |opt, val|
+        context["JSLintRB#{opt}"]  = val
+      end
+
+      context['JSLintRBinput'] = lambda { input }
+
+      context['JSLintRBerrors'] = lambda { |js_errors|
         js_errors.each do |e|
-          errors << "Error at line #{e['line'].to_i + 1} " + 
+          errors << "Error at line #{e['line'].to_i + 1} " +
             "character #{e['character'].to_i + 1}: #{e['reason']}"
           errors << "  #{e['evidence']}"
         end
       }
-      context.eval %{
-        JSLINT(JSLintRBinput(), {
-          adsafe   : JSLintRBadsafe,
-          bitwise  : JSLintRBbitwise,
-          browser  : JSLintRBbrowser,
-          cap      : JSLintRBcap,
-          css      : JSLintRBcss,
-          debug    : JSLintRBdebug,
-          eqeqeq   : JSLintRBeqeqeq,
-          evil     : JSLintRBevil,
-          forin    : JSLintRBforin,
-          fragment : JSLintRBfragment,
-          immed    : JSLintRBimmed,
-          laxbreak : JSLintRBlaxbreak,
-          newcap   : JSLintRBnewcap,
-          nomen    : JSLintRBnomen,
-          on       : JSLintRBon,
-          onevar   : JSLintRBonevar,
-          passfail : JSLintRBpassfail,
-          plusplus : JSLintRBplusplus,
-          regexp   : JSLintRBregexp,
-          rhino    : JSLintRBrhino,
-          undef    : JSLintRBundef,
-          safe     : JSLintRBsafe,
-          sidebar  : JSLintRBsidebar,
-          strict   : JSLintRBstrict,
-          sub      : JSLintRBsub,
-          white    : JSLintRBwhite,
-          widget   : JSLintRBwidget
-        });
-        JSLintRBreportErrors(JSLINT.errors);
-      }
+
+      # do it
+      context.eval [
+        "JSLINT(JSLintRBinput(), {",
+          @settings.map { |k,v| "#{k} : JSLintRB#{k}" }.join(",\n"),
+        "});",
+        "JSLintRBerrors(JSLINT.errors);"
+      ].join("\n")
     end
 
     if errors.empty?
