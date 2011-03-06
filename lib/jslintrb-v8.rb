75a8e7f463642e5e5cafd8d73988973dd99b07df
Rebecca Murphey <rebecca@rebeccamurphey.com>
2011-03-06 00:24:04 -0500

    check the right place for linter override

--- lib/jslintrb-v8.rb
+++ lib/jslintrb-v8.rb
@@ -109,7 +109,7 @@ class JSLint
       :widget     => false
     }
 
-    if @settings[:linter] == 'jshint'
+    if opts[:linter] == 'jshint'
       jshint_settings = {
         # tolerate automatic semicolon insertion
         :asi      => false,
